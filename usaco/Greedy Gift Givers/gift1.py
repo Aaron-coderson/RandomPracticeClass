a6970027dba444806cf9d8a28a6f5944b0b4560b
Aaron-coderson <51865907+Aaron-coderson@users.noreply.github.com>
2020-06-06 13:29:38 -0700

    tride

--- usaco/Greedy Gift Givers/gift1.py
+++ usaco/Greedy Gift Givers/gift1.py
@@ -1,10 +1,11 @@
 """
 ID: vijayaa1
-LANG: PYTHON2
+LANG: PYTHON3
 TASK: gift1
 """
 fin = open("gift1.in", "r")
-fout = open ('gift1.out', 'a')
+fout = open('gift1.out', 'w').close()
+fout = open('gift1.out', 'a')
 
 #get data into var
 x = fin.read().split('\n')
@@ -20,12 +21,14 @@ participant = {}
 for names in range(num_par):
     participant[data[0]] = 0
     data.pop(0)
-print(data)
+#print(data)
 
 while data != []:
     giver = data.pop(0)
+    if data == []:
+        break
     amt_rec = data.pop(0).split(' ')
-    print(amt_rec)
+#    print(amt_rec)
     amount = int(amt_rec[0])
     num_rec = int(amt_rec[1])
     participant[giver] -= amount
@@ -45,5 +48,4 @@ for part in participant:
 fout.close()
 
 print(participant)
-
 
